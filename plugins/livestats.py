5aae9144bf0547eb67a1afaa26a022a65688830e
Gurleen Singh <gs585@drexel.edu>
2022-11-14 16:29:12 -0500

    Revert "Updated queue in livestats to run from sync"
    This reverts commit 12d03a5c217c9c704e7b6377635cba043cf4c98d.

--- plugins/livestats.py
+++ plugins/livestats.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 
-from app import sio, sync_queue
+from app import sio, queue
 from store import STORE
 from loguru import logger
 from ncaa_live_stats import NCAALiveStats, Game
@@ -64,7 +64,9 @@ def update_home_player(game: Game):
         "homePlayerLine": line
     }
 
-    sync_queue.put({"sender": "NCAA Live Stats", "payload": payload})
+    asyncio.run_coroutine_threadsafe(
+        queue.put({"sender": "NCAA Live Stats", "payload": payload})
+    )
 
 
 stats.add_listener("boxscore", update_home_player)
